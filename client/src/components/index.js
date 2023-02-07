8ecd33d49cfbbd2baaf57db766d1c15ec4dc9c09
Chris Vaz <76993434+chris-vaz@users.noreply.github.com>
2023-02-07 18:03:13 +0300

    Completed Structure

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+// This file is used to export all the components
+import Card from './Card';
+import FormField from './FormField';
+import Loader from './Loader';
+
+export{
+    Card,FormField,Loader
+}
