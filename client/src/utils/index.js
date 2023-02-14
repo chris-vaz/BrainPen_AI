4f6d1c979c703d5fb9597f08fed972e0aa7735f7
Chris Vaz <76993434+chris-vaz@users.noreply.github.com>
2023-02-14 23:28:47 +0300

    updated create post jsx

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -2,3 +2,14 @@
 // is a file where we can use different functions which we can 
 // re use across different applications
 
+import {surpriseMePrompts} from '../constants';
+
+export function getRandomPrompt(prompt){
+    const randomIndex = Math.floor(Math.random()* surpriseMePrompts.length);
+
+    const randomPrompt = surpriseMePrompts[randomIndex];
+
+    if(randomPrompt === prompt) return getRandomPrompt(prompt);
+
+    return randomPrompt;
+}
